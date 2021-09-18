638ef12c01d73eb8f84bccab9cd698a920aafc1f
Lenar <linz14@mail.ru>
2021-09-18 14:38:40 +0300

    homepage

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,26 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
-document.addEventListener('DOMContentLoaded', () => {
+import 'jquery';
 
-	// Custom JS
-
-})
+$(document).ready(function(){
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    speed: 400,
+    slidesPerView: 2,
+    spaceBetween: 30,
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 0,
+      slideShadows: false,
+      depth: 860,
+      stretch: 0,
+    },
+    setWrapperSize: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+  });
+});
